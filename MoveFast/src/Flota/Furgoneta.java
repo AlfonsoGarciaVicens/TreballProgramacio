611031d05f72e54bd7c4e68a6fd759d5bc042ba2
KingFonso11 <47571086+KingFonso11@users.noreply.github.com>
2019-04-29 18:31:39 +0200

    Add files via upload
    Primera versió, amb Client i Flota(Classes)

---
+++ MoveFast/src/Flota/Furgoneta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Flota;
+
+/**
+ *
+ * @author Fonso
+ */
+public class Furgoneta extends Vehicle{
+    private String tipusFurgoneta;
+    private int cavalls;
+    public int numPortes;
+}
---
+++ MoveFast/src/Flota/Vehicle.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Flota;
+
+/**
+ *
+ * @author Fonso
+ */
+public class Vehicle {
+    private String matricula;
+    private int passatgers;
+    private String marca;
+    private String model;
+    private int tipusCarnet;
+    private double preuDia;
+    private String descripcio;
+    private String tipusAsseguranca;
+}
